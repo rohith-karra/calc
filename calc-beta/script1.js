061ca55c02b564aea92b8f3e4e55287d55b2f6a0
Rohith <57309698+rohith-karra@users.noreply.github.com>
2021-06-25 23:16:05 +0530

    Update script1.js

--- calc-beta/script1.js
+++ calc-beta/script1.js
@@ -1,15 +1,18 @@
 function add(i)
 		{
-			document.getElementById("result").value+=i
+			document.getElementById("result").value+=i;
 		}
 		
 function solve()
 		{
-			let x = document.getElementById("result").value
-			let y = eval(x)
-			document.getElementById("result").value = y
+			var x = document.getElementById("result").value;
+			var y = eval(x);
+			document.getElementById("result").value = y;
 		}
 function clr()
 		{
-			document.getElementById("result").value = ""
+			document.getElementById("result").value = "";
+		}
+		function back(){
+			document.getElementById("result").value=document.getElementById("result").value.slice(0,-1);
 		}
