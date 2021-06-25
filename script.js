190b6bd89f91b1a9870864a14bbdfb32f66f61f9
Rohith <57309698+rohith-karra@users.noreply.github.com>
2021-06-25 23:46:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,14 @@
-
-		function add(){
-			var a=parseInt(document.getElementById("number1").value);
-			var b=parseInt(document.getElementById("number2").value);
-			var x=a+b;
-			document.getElementById("result").innerHTML=x;
-			return false;
-		}
-		function sub(){
-			var a=document.getElementById("number1").value;
-			var b=document.getElementById("number2").value;
-			var x=a-b;
-			document.getElementById("result").innerHTML=x;
-			return false;
-		}
-		function mul(){
-			var a=document.getElementById("number1").value;
-			var b=document.getElementById("number2").value;
-			var x=a*b;
-			document.getElementById("result").innerHTML=x;
-			return false;
-		}
-		function div(){
-			var a=document.getElementById("number1").value;
-			var b=document.getElementById("number2").value;
-			var x=a/b;
-			document.getElementById("result").innerHTML=x;
-			return false;
-		}
-		function mod(){
-			var a=parseInt(document.getElementById("number1").value);
-			var b=parseInt(document.getElementById("number2").value);
-			var x=a%b;
-			document.getElementById("result").innerHTML=x;
-			return false;
-		}
 		function pow(){
 			var a=parseInt(document.getElementById("number1").value);
 			var b=parseInt(document.getElementById("number2").value);
 			var x=Math.pow(a,b);
-			document.getElementById("result").innerHTML=x;
-			return false;
-		}
-		function pow1(){
-			var a=parseInt(document.getElementById("number1").value);
-			var x=Math.pow(a,2);
-			document.getElementById("result").innerHTML=x;
+			document.getElementById("number2").value=x;
 			return false;
 		}
 		function sqrt(){
 			var a=parseInt(document.getElementById("number1").value);
 			var x=Math.sqrt(a);
-			document.getElementById("result").innerHTML=x;
+			document.getElementById("number2").value=x;
 			return false;
 		}
 		function fact(){
@@ -64,57 +22,48 @@
 				}
 			}  
 		
-			document.getElementById("result").innerHTML=x;
+			document.getElementById("number2").value=x;
 			return false;
 		}
 		function sin(){
 			var a=document.getElementById("number1").value;
 			var y= a*(Math.PI/180);
 			var x=Math.sin(y);
-			document.getElementById("result").innerHTML=x;
+			document.getElementById("number2").value=x;
 			return false;
 		}
 		function asin(){
 			var a=document.getElementById("number1").value;
 			var x=Math.asin(a);
 			var y= (x*(180/Math.PI)).toFixed(2);
-			document.getElementById("result").innerHTML=y;
+			document.getElementById("number2").value=y;
 			return false;
 		}
 		function cos(){
 			var a=document.getElementById("number1").value;
 			var y= a*(Math.PI/180);
 			var x=Math.cos(y);
-			document.getElementById("result").innerHTML=x;
+			document.getElementById("number2").value=x;
 			return false;
 		}
 		function acos(){
 			var a=document.getElementById("number1").value;
 			var x=Math.acos(a);
 			var y= (x*(180/Math.PI)).toFixed(2);
-			document.getElementById("result").innerHTML=y;
+			document.getElementById("number2").value=y;
 			return false;
 		}
 		function tan(){
 			var a=document.getElementById("number1").value;
 			var y= a*(Math.PI/180);
 			var x=Math.tan(y);
-			document.getElementById("result").innerHTML=x;
+			document.getElementById("number2").value=x;
 			return false;
 		}
 		function atan(){
 			var a=document.getElementById("number1").value;
 			var x=Math.atan(a);
 			var y= (x*(180/Math.PI)).toFixed(2);
-			document.getElementById("result").innerHTML=y;
+			document.getElementById("number2").value=y;
 			return false;
 		}
-function openTab(tabName) {
-  var i, x;
-  x = document.getElementsByClassName("containerTab");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "block";
-}
-
