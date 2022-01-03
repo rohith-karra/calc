cff776de5deac34fa7e9bc7b030774efe2578970
Rohith <57309698+rohith-karra@users.noreply.github.com>
2022-01-03 11:08:26 +0530

    Add files via upload

---
+++ convo/currency.js
@@ -0,0 +1,16 @@
+function convo(){
+ var a=document.getElementById("curr").value;
+ var b=a*100.45;
+ document.getElementById("res").value=b;
+}
+
+function convo1(){
+var a=document.getElementById("curr").value;
+ var b=a*74.41;
+ document.getElementById("res").value=b;
+}
+function convo2(){
+    var a=document.getElementById("curr").value;
+     var b=a*53.76;
+     document.getElementById("res").value=b;
+    }
