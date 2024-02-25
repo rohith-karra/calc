c0765664914ebf875763f7fd90777ade0e0e88a0
Rohith <57309698+rohith-karra@users.noreply.github.com>
2024-02-25 00:30:05 -0500

    health 1

--- convo/currency.js
+++ convo/currency.js
@@ -1,6 +1,6 @@
 function convo(){
  var a=document.getElementById("weight").value;
- var b=document.getElementById("height").value/100;
+ var b=document.getElementById("height").value;
  var c=a/b*b;
  document.getElementById("res").value=c;
 }
