3d7faf92401fae1f633a9404e8a22db695c600ce
Rohith <57309698+rohith-karra@users.noreply.github.com>
2024-02-25 00:27:31 -0500

    health

--- convo/currency.js
+++ convo/currency.js
@@ -1,16 +1,6 @@
 function convo(){
- var a=document.getElementById("curr").value;
- var b=a*100.45;
- document.getElementById("res").value=b;
+ var a=document.getElementById("weight").value;
+ var b=document.getElementById("height").value/100;
+ var c=a/b*b;
+ document.getElementById("res").value=c;
 }
-
-function convo1(){
-var a=document.getElementById("curr").value;
- var b=a*74.41;
- document.getElementById("res").value=b;
-}
-function convo2(){
-    var a=document.getElementById("curr").value;
-     var b=a*53.76;
-     document.getElementById("res").value=b;
-    }
