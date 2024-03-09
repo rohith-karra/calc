512050b24b29c835d44f682aa32271cc5d686d03
Rohith <57309698+rohith-karra@users.noreply.github.com>
2024-03-09 11:52:44 -0500

    ds

--- convo/bmi.js
+++ convo/bmi.js
@@ -2,6 +2,6 @@ function convo(){
  var a=document.getElementById("weight").value;
  var b=document.getElementById("height").value;
  var d=b/100;
- var c=a/Math.pow(d,2);
+ var c=(a/Math.pow(d,2)).toFixed(1);
  document.getElementById("res").value=c;
 }
