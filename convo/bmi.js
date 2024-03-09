daee1ad1aaaccee1f6a37c0beb5e2e2b3be08f8e
Rohith <57309698+rohith-karra@users.noreply.github.com>
2024-03-09 12:01:38 -0500

    status

--- convo/bmi.js
+++ convo/bmi.js
@@ -4,4 +4,16 @@ function convo(){
  var d=b/100;
  var c=(a/Math.pow(d,2)).toFixed(1);
  document.getElementById("res").value=c;
+ if(c<18.5){
+    document.getElementById("status").value="Under Weight";
+ }
+ else if(c>18.5 && c<24.9){
+    document.getElementById("status").value="Healthy keep it up";
+ }
+ else if(c>25 && c<29.9){
+    document.getElementById("status").value="Over Weight";
+ }
+ else{
+    document.getElementById("status").value="OMG Obese!! Please hit the Gym";
+ }
 }
